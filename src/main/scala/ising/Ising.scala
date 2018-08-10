956dee43f436ce20f79598b1ee2f2a215ac5a9a7
Nikshev <nikshev81@gmail.com>
2018-08-10 10:58:02 +0300

    Initial commit

---
+++ src/main/scala/ising/Ising.scala
@@ -0,0 +1,9 @@
+package ising
+
+object Ising extends Greeting with App {
+  println(greeting)
+}
+
+trait Greeting {
+  lazy val greeting: String = "hello world"
+}
---
+++ src/test/scala/ising/IsingSpec.scala
@@ -0,0 +1,9 @@
+package ising
+
+import org.scalatest._
+
+class IsingSpec extends FlatSpec with Matchers {
+  "The Ising object" should "say hello" in {
+    Hello.greeting shouldEqual "hello"
+  }
+}
